5f1c1b00b35aa1217f008b21d72d26016a83decc
Stanislav Iliev <stanislav.iliev@moneycorp.com>
2018-12-21 14:39:49 +0200

    Reimplement history using Elm debugger's data structure

--- src/Debug/Browser/History.elm
+++ src/Debug/Browser/History.elm
@@ -1,29 +1,50 @@
 module Debug.Browser.History exposing (History, init, insert, now)
 
+import Array exposing (Array)
 
-type alias Model model msg =
-    { initial : model
-    , entries : List ( msg, model )
+
+snapshotSize : Int
+snapshotSize =
+    5
+
+
+type alias History model msg =
+    { oldSnapshots : Array (Snapshot model msg)
+    , currentSnapshot : Snapshot model msg
     }
 
 
-type History model msg
-    = History (Model model msg)
+type alias Snapshot model msg =
+    { model : model
+    , messages : Array msg
+    , messageCount : Int
+    }
 
 
 init : model -> History model msg
 init model =
-    History
-        { initial = model
-        , entries = []
-        }
+    History Array.empty (emptySnapshot model)
 
 
 now : History model msg -> model
-now (History { initial, entries }) =
-    Maybe.withDefault initial (Maybe.map Tuple.second (List.head entries))
+now { currentSnapshot } =
+    currentSnapshot.model
 
 
 insert : ( msg, model ) -> History model msg -> History model msg
-insert entry (History model) =
-    History { model | entries = entry :: model.entries }
+insert (( _, model ) as entry) { oldSnapshots, currentSnapshot } =
+    if currentSnapshot.messageCount == snapshotSize then
+        History (Array.push currentSnapshot oldSnapshots) (insertSnapshotEntry (emptySnapshot model) entry)
+
+    else
+        History oldSnapshots (insertSnapshotEntry currentSnapshot entry)
+
+
+emptySnapshot : model -> Snapshot model msg
+emptySnapshot model =
+    Snapshot model Array.empty 0
+
+
+insertSnapshotEntry : Snapshot model msg -> ( msg, model ) -> Snapshot model msg
+insertSnapshotEntry snapshot ( msg, model ) =
+    Snapshot model (Array.push msg snapshot.messages) (snapshot.messageCount + 1)
