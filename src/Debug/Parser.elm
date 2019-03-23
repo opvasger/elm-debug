08b94748721c05dc812bee47869f8045ff7e25dd
opvasger <opvasger@gmail.com>
2019-03-23 22:28:57 +0100

    setting up for parsing output from Debug.toString

---
+++ src/Debug/Parser.elm
@@ -0,0 +1,12 @@
+module Debug.Parser exposing (Value, parser)
+
+import Parser exposing (Parser)
+
+
+type Value
+    = Value
+
+
+parser : Parser Value
+parser =
+    Debug.todo "not implemented yet"
